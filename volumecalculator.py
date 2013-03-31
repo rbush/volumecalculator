a8c01c93c20666982598993a8cd3a1697338aa83
rbush <ryan.bush@email.com>
2013-03-31 17:21:10 +0100

    v1.0

---
+++ volumecalculator.py
@@ -0,0 +1,6 @@
+print("Volume Calculator by Ryan Bush. v1.0")
+height = int(input("Enter your Height: "))
+width = int(input("Enter your Width: "))
+depth = int(input("Enter your Depth: "))
+volume = height * width * depth
+print("Volume: ", volume)
